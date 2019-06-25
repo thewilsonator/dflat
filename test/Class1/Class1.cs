db08db81bd7dfc7a4b51b659e980bce3e35fd7a5
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2019-06-25 16:43:44 +0800

    Get a better proxy of generated code. use override

--- test/Class1/Class1.cs
+++ test/Class1/Class1.cs
@@ -1,43 +1,46 @@
 using System;
 using System.Runtime.InteropServices;
+using System.IO;
 //namespace testlib
 //{
-    public class Class1
+public class Class1
+{
+    int a;
+    public Class1(int aa) { a = aa; }
+    public void foo() { a = 42; }
+    public override string ToString() { return a.ToString(); }
+}
+
+public class Class2
+{
+    public static IntPtr make(int a)
     {
-        int a;
-        public Class1(int aa) { a = aa; }
-        public void foo() { a = 42; }
-        public string ToString() { return a.ToString(); }
+        Object ret = new Class1(a);
+        GCHandle gch = GCHandle.Alloc(ret);
+        return GCHandle.ToIntPtr(gch);
     }
 
-    class Class2
+    public static string toString(IntPtr pthis)
     {
-        public static IntPtr make(int a)
-        {
-            var ret = new Class1(a);
-            GCHandle gch = GCHandle.Alloc(ret);
-            return GCHandle.ToIntPtr(gch);
-        }
-
-        public static string toString(IntPtr pthis)
-        {
-            var gch = GCHandle.FromIntPtr(pthis);
-            var targ = gch.Target;
-            Class2 actual = (Class2)targ;
-            return actual.ToString();
-        }
-        public static void unpin(IntPtr pthis)
-        {
-            GCHandle gch = GCHandle.FromIntPtr(pthis);
-            gch.Free();
-            return;
-        }
-        static void test()
-        {
-            var a = Class2.make(42);
-            string s = toString(a);
-            Class2.unpin(a);
-        }
+        //System.Console.WriteLine(pthis.ToString("X"));
+        var gch = GCHandle.FromIntPtr(pthis);
+        var targ = gch.Target;
+        //System.Console.WriteLine(().ToString());
+        Class1 actual = (Class1)targ;
+        return actual.ToString();
     }
+    public static void unpin(IntPtr pthis)
+    {
+        GCHandle gch = GCHandle.FromIntPtr(pthis);
+        gch.Free();
+        return;
+    }
+    static void test()
+    {
+        var a = Class2.make(42);
+        string s = toString(a);
+        Class2.unpin(a);
+    }
+}
 
 //}
